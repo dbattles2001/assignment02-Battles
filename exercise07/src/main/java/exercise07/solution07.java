324e284a58641751d5bf4707ba614a9f75f4d038
Dustin Battles <dbattles2001@knights.ucf.edu>
2021-09-17 14:15:36 -0400

    First real commit (day 1)

---
+++ exercise07/src/main/java/exercise07/solution07.java
@@ -0,0 +1,81 @@
+package exercise07;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 7
+ *  Copyright 2021 Dustin Battles
+ */
+
+/*
+    In this one we're gonna ask the user for their inputs, testing each time
+    using string parsing whether or not what theyre giving us is an INT.
+
+    Afterwords we will print the mathmatics.
+
+    For the one where it switches between feet and meters, i'll change it to have it ask for your selection first,
+    and then the part where it prints the conversion will be flipped according to that value
+    which will be like "meter = 1 or 0"
+ */
+
+public class solution07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int loop = 1, length = 0, width = 0, test = 0, choice;
+        double constant;
+        String[] types = new String[] {"length", "width"};
+
+        System.out.println("What would you like to measure in? 0 = feet, 1 = meters");
+        choice = input.nextInt();
+
+        for(int i = 0; i<2; i++){
+            test = 0;
+            while(test == 0)
+            {
+                System.out.println("Enter the " + types[i]);
+                String in = input.next();
+
+                if(numberTester(in))
+                {
+                    System.out.println("Thanks.");
+                    if(i == 0){
+                        length = Integer.parseInt(in);
+                    }
+                    else{
+                        width = Integer.parseInt(in);
+                    }
+                    test = 1;
+                }else{
+                    System.out.println("This ain't a number bro!!!!");
+                }
+            }
+        }
+
+        System.out.println("You entered the dimensions " + length + " and " + width);
+        int sqr = length * width;
+
+        if(choice == 0){
+            System.out.printf("Your square feet is %d\n", sqr);
+            double sqrTwo = sqr * 0.09290304;
+            System.out.printf("Your square meters is %f", sqrTwo);
+        }
+        else{
+
+            System.out.printf("Your square meters is %d\n", sqr);
+            double sqrTwo = sqr * 3.28084;
+            System.out.printf("Your square feet is %f", sqrTwo);
+        }
+    }
+
+    static boolean numberTester(String input)
+    {
+        try{
+            Integer.parseInt(input);
+        }
+        catch(NumberFormatException ex){
+            return false;
+        }
+        return true;
+    }
+}
---
+++ exercise08/src/main/java/exercise08/solution08.java
@@ -0,0 +1,74 @@
+package exercise08;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 8
+ *  Copyright 2021 Dustin Battles
+ */
+
+/*
+    In this one we're using a similar if not exact system to 7
+    where we take in multiple inputs, checking whether or not they're INTs while in a loop
+    calculate them and then print them
+ */
+
+public class solution08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int people = 0, slices = 0, want = 0, test, i = 0;
+        double pizzas;
+        String[] types = new String[] {"people", "slices per pizza", "slices each person wants"};
+
+        for(i = 0; i<3; i++){
+            test = 0;
+            while(test == 0)
+            {
+                System.out.println("Enter the amount of " + types[i]);
+                String in = input.next();
+
+                if(numberTester(in))
+                {
+                    System.out.println("Thanks.");
+                    switch(i)
+                    {
+                        case 0: people = Integer.parseInt(in);
+                        break;
+                        case 1: slices = Integer.parseInt(in);
+                        break;
+                        case 2: want = Integer.parseInt(in);
+                        break;
+                        default: System.exit(0);
+                        break;
+                    }
+                    test = 1;
+                }else{
+                    System.out.println("This ain't a number bro!!!!");
+                }
+            }
+        }
+
+        pizzas = ((people * 1.0 * want)/slices);
+        if(pizzas == 1)
+        {
+            System.out.println("Buy one pizza!");
+        } else{
+            System.out.println("The amount of pizza's you'll wanna order with");
+            System.out.printf("%d people who want %d slices where each pizza has %d slices is\n", people, want, slices);
+            System.out.println(pizzas);
+        }
+
+
+    }
+
+    static boolean numberTester(String input)
+    {
+        try{
+            Integer.parseInt(input);
+        }
+        catch(NumberFormatException ex){
+            return false;
+        }
+        return true;
+    }
+}
